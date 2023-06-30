429c101ea93d0f67e4b8083b8dde6ecd9cf9fd41
Ketakiii <59907464+ketakiii@users.noreply.github.com>
2023-06-30 15:34:27 -0400

    Add files via upload

--- src/Driver.java
+++ src/Driver.java
@@ -1,12 +1,11 @@
 import controller.Controller;
 import controller.Icontroller;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Random;
-
 import controller.IviewController;
 import controller.ViewController;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+import java.util.Scanner;
 import model.GameState;
 import model.IgameState;
 import view.DungeonPanel;
@@ -25,25 +24,29 @@ public class Driver {
    * @param args String
    */
   public static void main(String[] args) throws IOException {
-    Random random = new Random(12);
-    GameState gameState = new GameState(9, 10, 20, 30, 4, 4, false, 2, random);
-    //Menu menu = new Menu(gameState);
-    Iview view = new View(gameState);
-    IviewController controller = new ViewController(gameState, view);
-//    controller.playGame();
+    if (args.length == 0) {
+      System.out.println("Please Provide args - gui/text");
+      return;
+    }
+    if ("--gui".equals(args[0])) {
+      Random random = new Random(12);
+      GameState gameState = new GameState(9, 10, 20, 30, 4, 4, false, 2, random);
+      Iview view = new View(gameState);
+      IviewController viewcontroller = new ViewController(gameState, view);
+      viewcontroller.playGame(gameState);
+    } else if ("--text".equals(args[0])) {
+      Readable scanner = new InputStreamReader(System.in);
+      Appendable output = System.out;
+      Random random1 = new Random(12);
+      IgameState gameState1 = new GameState(9, 10, 20, 30, 4, 4, false, random1);
+      Icontroller controller = new Controller(scanner, output, gameState1);
+      controller.playGame();
+    } else {
+      throw new IllegalArgumentException("Please specify a valid argument");
+    }
 
-//    Readable scanner = new InputStreamReader(System.in);
-//    Appendable output = System.out;
-//    Random random = new Random(12);
-//    IgameState gameState = new GameState(9, 10, 20, 30, 4, 4, false, random);
-//    Icontroller controller = new Controller(scanner, output, gameState);
-    controller.playGame();
   }
 }
 
-// model initialization
-// put values in the model
 
-// create grid view
-// call controller and put the grid and model in controller - ViewController
 
